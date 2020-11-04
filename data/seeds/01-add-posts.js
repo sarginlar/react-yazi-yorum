3f2225eb5b251470adbd81fc256b9e2b42907eaf
RifatSargin <sarginlar@gmail.com>
2020-11-05 00:34:20 +0300

    deneme1

--- data/seeds/01-add-posts.js
+++ data/seeds/01-add-posts.js
@@ -6,21 +6,18 @@ exports.seed = function (knex) {
       // Inserts seed entries
       return knex("post").insert([
         {
-          title: "React Dersleri kanalina hosgeldiniz",
-          content:
-            "Bu kanalda React konulari ele alinmaktadir. Takip ettiginiz icin tesekkur ederiz.",
+          title: "Rifat sargın",
+          content: "Bu kanalda React konulari ele alinmaktadir. Takip ettiginiz icin tesekkur ederiz.",
           user_id: 1,
         },
         {
           title: "React bir UI (kullanici arayuzu) kutuphanesidir",
-          content:
-            "React sayesinde dinamik interaktif web uygulamari gelistirebilirsiniz.",
+          content: "React sayesinde dinamik interaktif web uygulamari gelistirebilirsiniz.",
           user_id: 1,
         },
         {
           title: "Redux bir state yonetim sistemidir",
-          content:
-            "Redux sayesinde karmasik state islemleri tek bir merkezden yonetilebilir.",
+          content: "Redux sayesinde karmasik state islemleri tek bir merkezden yonetilebilir.",
           user_id: 1,
         },
       ]);
